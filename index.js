68a5ec7f72e17be97a1600f4c0566add095c642b
annkim822@gmail.com Kim <annkim822@gmail.com>
2018-05-08 18:06:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,16 @@ sayHitoGrandma = "I can't hear you"
   if (string = lowercase) {
     return "I can't hear you!"
   }
-var sayHiToGranma = 
+var sayHiToGranma = "HELLO"
+sayHiToGranma = "YES INDEED!"
 }
+
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
