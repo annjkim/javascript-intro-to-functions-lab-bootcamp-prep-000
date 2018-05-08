aba819d9011e1469d70f4087e393dcab25d042e7
annkim822@gmail.com Kim <annkim822@gmail.com>
2018-05-08 17:48:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   var uppercase = "HELLO!"
-  uppercase = "no"
-  if (uppercase === uppercase) {
+  uppercase = "bye"
+  if (uppercase = "HELLO!") {
     return "YES INDEED!"
   }
